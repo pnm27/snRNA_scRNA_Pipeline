9d8cc58488512e90393de6ab830775c3de3b025b
pnm27 <47644233+pnm27@users.noreply.github.com>
2022-03-07 12:57:26 -0500

    Rectified small inconsistencies

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#BSUB -J Dual_pilot-Snakemake
+#BSUB -J Gen_Snakemake
 #BSUB -P acc_CommonMind
 #BSUB -q premium
 #BSUB -n 1
 #BSUB -R span[hosts=1]
-#BSUB -R rusage[mem=5000]
+#BSUB -R rusage[mem=1000]
 #BSUB -W 48:00
 #BSUB -oo /sc/arion/projects/psychAD/snakemake_pipeline/Snakemake.stdout
 #BSUB -eo /sc/arion/projects/psychAD/snakemake_pipeline/Snakemake.stderr
@@ -17,5 +17,5 @@ ml R/4.1.0
 
 source /sc/arion/work/prashf01/conda/envs/snakemake/etc/profile.d/conda.sh
 conda activate snakemake
-snakemake --profile prachu_lsf --restart-times 3 --latency-wait 10 #--batch all=1/3
+snakemake --profile prachu_lsf --restart-times 2 --latency-wait 10 #--batch all=1/3
 #snakemake -j 4 --restart-times 2 --latency-wait 10
