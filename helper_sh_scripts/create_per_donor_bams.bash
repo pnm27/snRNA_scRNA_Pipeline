b42c8585a9e36beb0e9dc703d003ccaa8c1ce759
pnm27 <47644233+pnm27@users.noreply.github.com>
2022-03-07 12:20:30 -0500

    Modified split-bam for chr1 to be more efficient

---
+++ helper_sh_scripts/create_per_donor_bams.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# ${1} is the donor name
+# ${2} is the hash_file with donor name and the cell barcodes (with header, tab separated)
+# ${3} is temp dir (with sample name as the parent dir)
+# ${4} is the output dir (with sample name as the parent dir)
+# ${5} is the multiplexed bam file
+
+set -x 
+ml samtools
+
+awk -v donor="${1}" '(NR> 1 & $1 == donor){print $2}' ${2} > "${3}${1}.txt"
+samtools view -D CB:"${3}${1}.txt" ${5} -bho "${4}${1}.bam"
