9889d7212740cac7c1b832d5271956779903396f
pnm27 <47644233+pnm27@users.noreply.github.com>
2022-04-20 17:31:34 -0400

    Add executor script for SNV_aware Snakefile

---
+++ run_snakemake_snv_aware.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#BSUB -J Gen_Snakemake
+#BSUB -P acc_CommonMind
+#BSUB -q premium
+#BSUB -n 1
+#BSUB -R span[hosts=1]
+#BSUB -R rusage[mem=1000]
+#BSUB -W 48:00
+#BSUB -oo /sc/arion/projects/psychAD/pnm/Snakemake.stdout
+#BSUB -eo /sc/arion/projects/psychAD/pnm/Snakemake.stderr
+#BSUB -L /bin/bash
+
+
+ml anaconda3
+ml R/4.1.0
+
+source /sc/arion/work/prashf01/conda/envs/snakemake/etc/profile.d/conda.sh
+conda activate snakemake
+snakemake --profile prachu_lsf --restart-times 2 --latency-wait 10 -s Snakefile_gt_ase #--batch all=1/3
+#snakemake -j 4 --restart-times 2 --latency-wait 10
