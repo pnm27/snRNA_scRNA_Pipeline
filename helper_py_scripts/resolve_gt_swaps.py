b8bdba6860386c2f5c8d65eb81cd59d5eac5d2b2
pnm <pnm27@gwmail.gwu.edu>
2022-11-15 17:23:58 -0500

    Temp fix to resolve already created gt_swaps_resolved donor h5ads

--- helper_py_scripts/resolve_gt_swaps.py
+++ helper_py_scripts/resolve_gt_swaps.py
@@ -11,18 +11,21 @@ ref_f=pd.read_csv("sample_to_donor_convertor_myUpdate_ver3.csv")
 out_dir="/sc/arion/projects/psychAD/gt_swaps_resolved_donor_h5ad/"
 
 for s in ref_f['SubID'].unique():
-    files_list_df = ref_f.loc[ref_f['SubID'] == s, ["SubID", "orig_pool", "file_n", "orig_id"]]
-    adatas = []
-    for i, j in files_list_df.iterrows():
-        temp = ad.read(j['file_n'])
-        temp = temp[temp.obs['SubID_vS'] == j['orig_id']].copy()
-        temp.obs['SubID_vS'] = j['SubID']
-        temp.obs['poolID_ref'] = j['orig_pool']
-        adatas.append(temp)
+    # Only if the donor files are absent then create it
+    # Modify it later
+    if not os.path.isfile(os.path.join(out_dir, f"{s}.h5ad")):
+        files_list_df = ref_f.loc[ref_f['SubID'] == s, ["SubID", "orig_pool", "file_n", "orig_id"]]
+        adatas = []
+        for i, j in files_list_df.iterrows():
+            temp = ad.read(j['file_n'])
+            temp = temp[temp.obs['SubID_vS'] == j['orig_id']].copy()
+            temp.obs['SubID_vS'] = j['SubID']
+            temp.obs['poolID_ref'] = j['orig_pool']
+            adatas.append(temp)
+            
+        adata = ad.concat(adatas[:], index_unique='-', join='outer') # it will add a 'batch' variable with keys as its value and will outer join on the 'genes'
         
-    adata = ad.concat(adatas[:], index_unique='-', join='outer') # it will add a 'batch' variable with keys as its value and will outer join on the 'genes'
-    
-    adata.write(os.path.join(out_dir, f"{s}.h5ad"))
+        adata.write(os.path.join(out_dir, f"{s}.h5ad"))
 
-    del adatas, adata, temp
-    gc.collect()
+        del adatas, adata, temp
+        gc.collect()
