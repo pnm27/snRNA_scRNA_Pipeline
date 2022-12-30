e0e2ab5411c12e2c91eeb5123478c4715dc03f75
pnm <pnm27@gwmail.gwu.edu>
2022-12-30 15:34:41 -0500

    First major bug fix for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,12 +31,14 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser"
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosectionlabel",
 ]
 
-source_suffix = [
-    ".md",
-]
+# source_suffix = [
+#     ".md",
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
