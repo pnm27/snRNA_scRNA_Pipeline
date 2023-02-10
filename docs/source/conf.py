fe0019543daf16b42b9faac90e3718413a57fa17
pnm <pnm27@gwmail.gwu.edu>
2023-02-10 11:08:59 -0500

    Test rtd issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../helper_py_scripts/'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
